d72d335253c951d9f5c7ed181450cf5c953aa5a3
unknown <souhail7ouabou@gmail.com>
2022-01-27 18:22:57 +0100

    setup layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {Switch,Route,Link } from 'react-router-dom';
-import { Layout, Typgraphy, Space } from 'antd';
-import { Navbar } from './components';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from 'antd';
+import { Navbar,Exchanges,CryptoDetails,Cryptocurrencies,News,Homepage } from './components';
 import './App.css'
 const App = () => {
   return (  
@@ -10,12 +10,30 @@ const App = () => {
             <Navbar/>
           </div>
           <div className="main">
-
-          </div>
+            <Layout>
+              <div className="routes">
+                <Routes>
+                     <Route path="/" exact element={ Homepage} />
+                     <Route path="/exchanges"  element={Exchanges} />
+                     <Route path="/cryptocurrencies"  element={Cryptocurrencies} />
+                     <Route path="/crypto/:coiId"  element={CryptoDetails} />
+                     <Route path="/news"  element={News} />
+                </Routes>
+              </div>
+            </Layout>
+          
           <div className="footer">
-
+              <Typography.Title level={5} style={{color:'white' , textAlign: 'center'}}>
+                  CryptoUnivers <br/>
+                  All Right reserverd
+              </Typography.Title>
+              <Space>
+                <Link to="/">Home</Link>
+                <Link to="/exchanges">Exchanges</Link>
+                <Link to="/news">News</Link>
+              </Space>
           </div>
-
+         </div>
     </div>
     )};
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,13 +19,13 @@ const Navbar = () => {
                     <Link to="/">Home</Link>
                 </Menu.Item>
                 <Menu.Item icon={<FundOutlined/>}>
-                    <Link to="/cryptocurrencies">Vryptocurrencies</Link>
+                    <Link to="/cryptocurrencies">Cryptocurrencies</Link>
                 </Menu.Item>
                 <Menu.Item icon={<MoneyCollectOutlined/>}>
                     <Link to="/exchanges">Exchanges</Link>
                 </Menu.Item>
                 <Menu.Item icon={<BulbOutlined/>}>
-                    <Link to="/mews">News</Link>
+                    <Link to="/news">News</Link>
                 </Menu.Item>
             </Menu>
         </div>
