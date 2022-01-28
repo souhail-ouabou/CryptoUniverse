3aef1a3d42cf717f1c3e7d714da1255bf940fc76
souhail-ouabou <souhail7ouabou@gmail.com>
2022-01-28 19:12:12 +0100

    Cryptocurrencies page done

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
-
-const Cryptocurrencies = () => {
-  return <div>
-      Cryptocurrencies
-  </div>;
+import React, { useEffect, useState } from 'react';
+import millify from 'millify';
+import {Link} from 'react-router-dom';
+import { useGetCryptosQuery } from '../services/cryptoApi';
+import { Card, Col, Input, Row } from 'antd';
+const Cryptocurrencies = ({simplified}) => {
+  const count = simplified ? 10 : 100;
+  const { data : cryptoList, isFetching} = useGetCryptosQuery(count);
+  const [cryptos, setCryptos] = useState([]);
+  const [searchTerm,setSearchTerm] = useState('');
+  useEffect(() => {
+    const filtredData = cryptoList?.data?.coins.filter((coin)=> coin.name.toLowerCase().includes(searchTerm.toLocaleLowerCase()));
+    setCryptos(filtredData);
+  },[cryptoList,searchTerm]);
+  if(isFetching) return 'Loading...'
+ return( <>
+ {!simplified && (
+    <div className="search-crypto">
+            <Input placeholder='Search Cryptocurrency' onChange={(e)=>setSearchTerm(e.target.value)} />
+      </div>
+      )}  
+      <Row gutter={[32,32]} className='crypto-card-container'>
+          {cryptos?.map((crypto)=>(
+            <Col key={crypto.uuid} xs={24} sm={12} lg={6} className='crypto-card'>
+              <Link  key={crypto.uuid} to={`/crypto/${crypto.uuid}`}>
+                  <Card
+                 title={`${crypto.rank}. ${crypto.name}`}
+                 extra={<img className="crypto-image" src={crypto.iconUrl} />}
+                 hoverable>
+                <p>Price: {millify(crypto.price)}</p>
+                <p>Market Cap: {millify(crypto.marketCap)}</p>
+                <p>Daily Change: {crypto.change}%</p>
+                 </Card>
+              </Link>
+            </Col>
+          ))}
+      </Row>
+  </>);
 };
 
 export default Cryptocurrencies;
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -15,7 +15,7 @@
     baseQuery: fetchBaseQuery({ baseUrl}),
     endpoints: (builder) => ({
       getCryptos: builder.query({
-        query: () => creatRequest('/coins ')
+        query: (count) => creatRequest(`/coins?limit=${count}`)
       }),
     }),
   });
